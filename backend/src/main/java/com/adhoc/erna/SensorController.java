9a8fbd8cfeecdf7e355f4a54f11cb6e574eb3798
Thilko Richter <me@thilko.com>
2018-07-29 07:51:45 +0200

    introduce different folders

--- backend/src/main/java/com/adhoc/erna/SensorController.java
+++ backend/src/main/java/com/adhoc/erna/SensorController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sensor")
+@RequestMapping("/api/sensor")
 public class SensorController {
 
     private final SensorRepository sensorRepository;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,30 @@ import './App.css';
 
 class App extends Component {
 
+    constructor(props){
+        super(props)
+        this.state = {
+            testMe: "test XXX"
+        }
+    }
+
+    bla = () => {
+        fetch('/api/sensor/8773887')
+            .then(function(response) {
+                return response.text();
+            })
+            .then(myJson => {
+                console.log(myJson)
+                this.setState({
+                    testMe: myJson
+                })
+            });
+    }
+
   componentDidMount(){
-    fetch('/sensor/8773887')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(myJson) {
-        console.log(myJson);
-      });
+    this.bla()
   }
+
   render() {
     return (
       <div className="App">
@@ -22,6 +37,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
         </p>
+
+          <div>{this.state.testMe}</div>
       </div>
     );
   }
